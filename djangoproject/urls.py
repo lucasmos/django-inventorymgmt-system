bbf51ef408351f9f61db262b6a395c7d5c0a4935
Luke Mosoti <lukemosoti19@gmail.com>
2024-04-30 16:38:39 +0300

    modified settings

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from stockmgmt import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
 	path('', views.home, name='home'),
@@ -60,5 +59,3 @@ urlpatterns = [
     path('staff/', views.staff, name="staff"),
     path('order/', views.order, name='order'),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
